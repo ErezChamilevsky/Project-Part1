f1aa351c6f5be17d2c8b2e7559e6f89431fe7d15
Itamar Berger <Itamark951@gmail.com>
2024-05-29 16:01:59 +0300

    Update json file with picture

--- web/footube/src/video_list_screen/Video/Video.js
+++ web/footube/src/video_list_screen/Video/Video.js
@@ -1,10 +1,10 @@
 import "./Video.css"
-import cookies from "../../data/pictures/cookies.jpg"
+
 function Video({id,img, title, artist, publication_date ,views, category}){
 return(
     <a href="video.html?id ={id}">
     <div class = "video_detail">
-    <img class = "video_photo" src={cookies}/>
+    <img class = "video_photo" src={img}/>
             <div>{title}</div>
             <div>{artist}</div>
             <time>{publication_date}</time>
