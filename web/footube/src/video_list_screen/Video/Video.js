913d6d3667c2080509dafc504a5546cb07892789
Itamar Berger <Itamark951@gmail.com>
2024-05-30 12:05:11 +0300

    Another simple adjustmant of vidos grid

--- web/footube/src/video_list_screen/Video/Video.js
+++ web/footube/src/video_list_screen/Video/Video.js
@@ -2,20 +2,18 @@ import "./Video.css"
 import { Link } from "react-router-dom";
 function Video({id,img, title, artist, publication_date ,views, category}){
 return(
-    <div class ="col-xl-2 col-lg-3 col-md-4 col-sm-6 border-0">
     <div class="card" styles="width: 18rem">
         <Link to = "html.?id{id}">
-  <img src={img} class="card-img-top" alt="..."/>
+  <img src={img} className="card-img-top" alt="Responsive image"/>
   <div class="card-body">
   <p class="card-text"> {title}</p>
-    <p class="card-text"> {artist}</p>
-    <div class="card-footer">
-      <small class="text-body-secondary">views | {publication_date}</small>
+    <div class="card-footer" styles="background-color:trasparent">
+    <div class="artist"> {artist}</div>
+      <text class="text-body-secondary">views | {publication_date}</text>
   </div>
 </div>
   </Link>
 </div>
-   </div>
 );
 }
 export default  Video;
--- web/footube/src/video_list_screen/vid_list/Vid_list.js
+++ web/footube/src/video_list_screen/vid_list/Vid_list.js
@@ -7,15 +7,17 @@ import { useState } from 'react';
 function Vid_list(){
 const [videosList, setVideoList] = useState(videos);
 return(
+    <div class ="container">
     <div class="row row-cols-1 row-cols-md-4 g-6 ">{
         videosList.map((video)=>
-            <div class="col">
+            <div class="col-md-4">
                    <div class="card h-100">
                 <Video {...video}/>
                 </div>
-                </div>
+           </div>
 )
 }
+</div>
 </div>
 ); 
 
