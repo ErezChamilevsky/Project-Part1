6f03419534f391da2cf8b4da9417f8b001972825
Itamar Berger <Itamark951@gmail.com>
2024-05-30 22:36:26 +0300

    All 10 vidos have been updated into json file

--- web/footube/src/video_list_screen/sidebar/sideBar.js
+++ web/footube/src/video_list_screen/sidebar/sideBar.js
@@ -18,15 +18,15 @@ function SideBar(){
     <div class="list-group" id="list-tab" role="tablist">
       <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">
       
-      <li  id = "sidenav">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
-    <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
-  </svg>
 
-    Home
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+      <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+      <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+      </svg>
+    
+       Home
   
-  </li>   
+  
       </a>
       <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">
          <li  id = "sidenav">
@@ -73,7 +73,11 @@ function SideBar(){
          
           </ul>
         </li>
-    <a class="navbar-brand" href="#">Footube</a>
+    <a class="navbar-brand" href="#">
+      <img class="nav-item" src = "./Footube.png " type="icon"/>
+     </a>
+      <a class="navbar-brand" href="#">
+      Footube</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
