8c152853266d4531790b84cf9c1d3dbc8e16d10f
Erez <erez284@gmail.com>
2024-06-10 19:28:08 +0300

    changing some videos name for oder, handeling the rerendering of the video's details and vid_src

--- web/footube/src/App.js
+++ web/footube/src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Watch from './watching_Screen/watchingPage';
 import vidData from './data/vid.json'; //needed to change to state
 import userDataList from './data/user.json'; //needed to change to state
-import commentsDataList from './data/comments.json';
-
 import Login from './pages/Login/login.js';
 import Register from './pages/Register/Register.js';
 import AddNewVideoScreen from './pages/AddNewVideoScreen/AddNewVideoScreen.js';
@@ -15,8 +13,8 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [users, setUsers] = useState([]); // define users array
-  const [videos, setVideos] = useState([]);
+  const [users, setUsers] = useState(userDataList); // define users array
+  const [videos, setVideos] = useState(vidData);
 
   return (
     <div>
@@ -24,7 +22,7 @@ function App() {
         <Route path="/" element={ <Login users={users}/> } />
         <Route path='/register' element={ <Register users={users} setUsers={setUsers}/> }></Route>
         <Route path='/addNewVideoScreen' element={ < AddNewVideoScreen /> }></Route>
-        <Route path='/watch/:vid_id' element={<Watch videoDataList={vidData} userDataList={userDataList} commentsDataList={commentsDataList} />} />
+        <Route path='/watch/:vid_id' element={<Watch videoDataList={videos} userDataList={users}/>} />
        </Routes>
     </div>
 
--- web/footube/src/watching_Screen/watchingPage.js
+++ web/footube/src/watching_Screen/watchingPage.js
@@ -1,72 +1,59 @@
-import './watchingPage.css'
+import './watchingPage.css';
 import Details from './details-section/details';
 import Like_toolbar from './like-toolbar/like-toolbar';
 import VideoList from './video-list/video-list';
 import CommentSection from './comments-section/commentsSection';
 import { useParams } from 'react-router-dom';
 import VideoDisplay from './video_display/videoDisplay';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import commentsDataList from '../data/comments.json';
 
-
-function Watch({ videoDataList, userDataList, commentsDataList }) {
-
+function Watch({ videoDataList, userDataList }) {
   const { vid_id } = useParams();  // Extract vid_id from useParams
-
   const intId = parseInt(vid_id, 10);
 
   const [commentList, setCommentList] = useState(commentsDataList);
+
+  let currentVideoFromVideoList = videoDataList.find(video => video.id == intId);
+  // console.log({currentVideoFromVideoList});
+  let currentUser = userDataList[0];
   
   
 
-  // Find the video object with the provided vid_id
-  const currentVideoFromVideoList = videoDataList.find(video => video.id === intId);
-
-  if (!currentVideoFromVideoList) {
-    return <div>Video not found</div>;  
-  }
-
-  // Find the user object with the same user_id as the current video
-  const currentUser = userDataList.find(user => user.user_id === currentVideoFromVideoList.user_id);
-  if (!currentUser) {
-    return <div>User not found</div>;  // You can return a message or handle this case accordingly
-  }
 
   return (
     <div className="container">
+
       <div className="up-toolbar">
-        <div className="up-toolbar">
-          <p>this is a tool bar {currentVideoFromVideoList.likes} {currentVideoFromVideoList.video_src}</p>
+        <p>this is a tool bar {currentVideoFromVideoList.likes} {currentVideoFromVideoList.video_src}</p>
+      </div>
+      <div className='video-container'>
+
+        <div className='left-block'>
+          <div className='vid-display'>
+            <VideoDisplay vid_src={currentVideoFromVideoList.video_src}/>
+          </div>
+          <div className='descriptions'>
+            <div className='vidTitle'>
+              <h4>{currentVideoFromVideoList.title}</h4>
+            </div>
+            <Like_toolbar
+              userName={currentUser.user_name}
+              userImg={currentUser.user_img}
+              vidLikes={currentVideoFromVideoList.likes}
+            />
+            <Details
+              details={currentVideoFromVideoList.details}
+              viewsNum={currentVideoFromVideoList.views}
+              uploadDate={currentVideoFromVideoList.publication_date}
+            />
+            <div className='comment-section'>
+              <CommentSection commentList={commentList} setCommentList={setCommentList} />
+            </div>
+          </div>
         </div>
-        <div className='video-container'>
-          <div className='left-block'>
-            <div className='vid-display'>
-              <VideoDisplay video_src={currentVideoFromVideoList.video_src}/>
-            </div>
-            <div className='descriptions'>
-              <div className='vidTitle'>
-                <h4>{currentVideoFromVideoList.title}</h4>
-              </div>
-              {/* Pass user data to Like_toolbar */}
-              
-              <Like_toolbar
-                userName={currentUser.user_name}
-                userImg={currentUser.user_img}
-                vidLikes={currentVideoFromVideoList.likes}
-              />
-              <Details
-                details={currentVideoFromVideoList.details}
-                viewsNum={currentVideoFromVideoList.views}
-                uploadDate={currentVideoFromVideoList.publication_date}
-              />
-              <div className='comment-section'>
-                {/* <AddComment></AddComment> */}
-                <CommentSection commentList={commentList} setCommentList={setCommentList} />
-              </div>
-            </div>
-          </div>
-          <div className='right-block-vid-list'>
-            <VideoList videoData={videoDataList} />
-          </div>
+        <div className='right-block-vid-list'>
+          <VideoList videoData={videoDataList} />
         </div>
       </div>
     </div>
