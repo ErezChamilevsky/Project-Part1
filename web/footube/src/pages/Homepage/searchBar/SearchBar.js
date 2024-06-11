e1ac576a0868bb4d97b453728517ed0e1515a202
San Haviv <sanhaviv95@gmail.com>
2024-06-11 17:22:21 +0300

    modified:   web/footube/package-lock.json 	modified:   web/footube/src/App.js 	new file:   web/footube/src/pages/Homepage/Homepage.css 	new file:   web/footube/src/pages/Homepage/Homepage.html 	new file:   web/footube/src/pages/Homepage/Homepage.js 	new file:   web/footube/src/pages/Homepage/footubelogo.png 	new file:   web/footube/src/pages/Homepage/searchBar/SearchBar.js 	new file:   web/footube/src/pages/Homepage/sideBar/SideBar.js 	new file:   web/footube/src/pages/Homepage/videoItem/VideoItem.js 	modified:   web/footube/src/pages/Register/Register.js
     Changes not staged for commit:
    	modified:   web/footube/src/pages/Homepage/Homepage.js
    	modified:   web/footube/src/pages/Homepage/searchBar/SearchBar.js
    	modified:   web/footube/src/pages/Register/Register.js

---
+++ web/footube/src/pages/Homepage/searchBar/SearchBar.js
@@ -0,0 +1,48 @@
+import '../Homepage.css'
+import videos from '../../../data/vid.json'
+
+
+function SearchBar({setCurrentVideos}) {
+
+
+    function handleSearch() {
+        let searchInput = document.getElementById('search_bar_Homepage').value;
+        sessionStorage.setItem('currentSessionSearch', searchInput);
+    }
+
+    function liozGay() {
+        event.preventDefault();
+        let lastInput = sessionStorage.getItem('currentSessionSearch')
+        const filteredVideos = videos.filter(video => video.title.toLowerCase().includes(lastInput.toLowerCase()))
+        // alert(filteredVideos.length)
+        setCurrentVideos(filteredVideos)
+    }
+
+
+    return (
+        <div className="header_Homepage">
+            <div className="header__left_Homepage" position="relative">
+                <i id="menu" className="material-icons">menu</i>
+                <div id="youtubeLogo">
+                </div>
+                Youtube
+            </div>
+            <div className="header__search_Homepage">
+                <form action="">
+                    <input id="search_bar_Homepage" type="text" placeholder="Search" onChange={handleSearch} />
+                    <button onClick={liozGay}><i className="material-icons">search</i></button>
+                </form>
+            </div>
+            <div className="header__icons_Homepage">
+                <button onClick="login()">Login</button>
+                <button onClick="register()">Register</button>
+                <i className="material-icons display-this">search</i>
+                <i className="material-icons display-this">account_circle</i>
+            </div>
+        </div>
+    );
+
+}
+
+
+export default SearchBar
