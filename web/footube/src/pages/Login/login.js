42174c6921f8ba336a7b02bc09844d48c633ce8c
Izhak Ben David <izhak573@gmail.com>
2024-06-30 20:05:55 +0300

    adjust login page code to server

--- web/footube/src/pages/Login/login.js
+++ web/footube/src/pages/Login/login.js
@@ -3,79 +3,98 @@ import { Link,useNavigate } from 'react-router-dom';
 
 import './login.css'; 
 
-function Login({ users, setLoggedUser,loggedUser }) {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [userFound, setUserFound] = useState(null); //this state for visualtion if the user founnd or not.
+function Login() {
 
-  const navigate = useNavigate(); // Initialize the navigate function
+    const [errorMessages, setErrorMessages] = useState('')  // hold the error messages that return from the server
+    const [loginUser, setLoginUser] = useState({userName: '', userPassword: ''});
+
+    const navigate = useNavigate(); // Initialize the navigate function
+    
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setLoginUser({ ...loginUser, [name]: value });
+        console.log(loginUser); // to check the login user object
+        }
 
 
-  //fuction that handle with user that want to enter to site , and check if he exist
-  const handleLogin = (event) => {
-      event.preventDefault(); // Prevent form submission and prvent delete the users state
-      // Check if the entered username and password match any user in the users array
-      const userExists = users.find(user => user.userName === username && user.userPassword === password);
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://localhost:12345/api/tokens', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginUser)
+            });
+            const data = await response.json();
+            if (response.status === 404) { //login faild
+                setErrorMessages(data.message || '');  //extarct the error messages from the response
+            } else {  //login is success
+                setErrorMessages('');  //clear the error messages
+                localStorage.setItem('token', data.token); // Save the token in local storage 
+                setLoginUser({   //clear the input fields
+                  userName: '',
+                  userPassword: '',   
+                });
+                navigate('/'); // Navigate to the home page on successful login
+        }  
+        
+        } catch (error) {
+            console.log('Error during login:', error);
+        }
 
-      const userFound = false; // Assume the user doesn't exist for demonstration
-
-      if (userExists) {
-        const userFound = true; // Assume the user doesn't exist for demonstration
-        setUserFound(userFound);
-        setLoggedUser(userExists); //define the loggedUser state be current user who log in
-        navigate('/'); // Navigate to the home page on successful login
-        console.log(userExists);
-        console.log(loggedUser);
-      } else {
-        setUserFound(userFound);
-      }
-  };
-
+    }
  
-
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <div className="login-card">
-        <h2 className="text-center login-title">Login</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              className="form-control"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)} // Update state on input change
-              required
-            />
-           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              className={`form-control`}
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} // Update state on input change
-              required
-            />
-          </div>
-          <button onClick={handleLogin} type="submit" className="btn btn-danger btn-block login-button ">
-            Login
-          </button>
-            {/* this visualition if userName or password are wrong*/}
-            {userFound === null ? null : userFound ? (
-              <div></div>)
-             : (
-              <div style={{ color: 'red' }}>UserName or password wrong</div>
+    
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+          <div className="login-card">
+            <h2 className="text-center login-title">Login</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="userName"
+                  value={loginUser.userName}
+                  onChange={handleInputChange} // Update state on input change
+                  required
+                />
+               </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className={`form-control`}
+                  name="userPassword"
+                  value={loginUser.userPassword}
+                  onChange={handleInputChange} // Update state on input change
+                  required
+                />
+              </div>
+              <button onClick={handleLogin} type="submit" className="btn btn-danger btn-block login-button ">
+                Login
+              </button>
+                {/* this visualition if userName or password are wrong*/}
+                {errorMessages && (
+                <div className="form-group row">
+                    <div className="col-sm-10">
+                        <small className="text-error" style={{ color: 'red', fontWeight: 'bold' }}>
+                            {errorMessages}
+                        </small>
+                    </div>
+                </div>
             )}
-          <p className="not-registered ">Not registered?</p>    
-              <Link to='/register' className="cr-acc btn btn-info registerButton">Create an account</Link>    
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default Login;
+              <p className="not-registered ">Not registered?</p>    
+                  <Link to='/register' className="cr-acc btn btn-info registerButton">Create an account</Link>    
+            </form>
+          </div>
+        </div>
+      );
+    };
+    
+    export default Login;
+    
