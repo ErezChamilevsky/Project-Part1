128d9bfe397180cabca0994d15810e4ac8030baf
Izhak Ben David <izhak573@gmail.com>
2024-07-02 18:36:18 +0300

    add functionality of delete user to client side

--- web/footube/src/pages/Homepage/searchBar/SearchBar.js
+++ web/footube/src/pages/Homepage/searchBar/SearchBar.js
@@ -25,7 +25,46 @@ function SearchBar({setCurrentVideos, loggedUser, setLoggedUser}) {
         setLoggedUser(null);
     }
 
+    //this function show pop up message to confirm the user want to delete the account
+    function checkIfDeleteUser(){
+        var userConfirmed = confirm("Are you sure you want to delete your account?");
+        if (userConfirmed) {
+            // Proceed with deleting the user account
+            handleDeleteUser();
+        } else {
+            // User cancelled, do nothing
+            console.log("User cancelled the account deletion.");
+        }
+    }
+    
 
+    //this function delete the user account from mongoDB.
+    const handleDeleteUser = async () => {
+        try {
+            const response = await fetch('http://localhost:12345/api/users/' + loggedUser.userId ,{
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': 'bearer ' + localStorage.getItem('token') // attach the token
+                },
+            });
+            const data = await response.json();
+            if (response.status === 404) {
+                var serverMsg = confirm(data.message) //extarct the server messages from the response
+            } else { //update success 
+                var serverMsg = confirm(data.message) //extarct the server messages from the response
+                setLoggedUser(null); //set logged user to null
+                localStorage.removeItem('token'); // Clears a token of logges user from local storage     
+        }  
+        
+        } catch (error) {
+            console.log('Error during details update:', error);
+        }
+    };
+
+
+
+// if no logged user, display the only login button
 if(!loggedUser){
     return (
         <div className="header_Homepage">
@@ -79,8 +118,9 @@ if(!loggedUser){
                 
                <div><Link to='/addNewVideoScreen' className="cr-acc btn btn-info registerButton">Add New Video</Link></div>    
                 <div><Link to='/login' className="cr-acc btn btn-info registerButton login-btn">Login</Link></div>
-                <div><Link to='/updateUser' className="cr-acc btn btn-info registerButton login-btn">Update Details</Link></div>    
                 <div><button onClick={handleLogOut} className="cr-acc btn btn-info registerButton">Log Out</button></div>
+                <div><Link to='/updateUser' className="cr-acc btn btn-info registerButton login-btn">Update Details</Link></div>    
+                <div><button onClick={checkIfDeleteUser} className="cr-acc btn btn-info registerButton">Delete user</button></div>
 
                 {/* this lines display the userName and the image in right side of search bar*/}
                 {loggedUser ? (
